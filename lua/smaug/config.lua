eec8f8edbfbd11798e7989c7b5d93e698e2e2158
Smaug6739 <veauville.raphael@gmail.com>
2022-06-14 18:17:35 +0200

    Add vuejs framework configs

--- lua/smaug/config.lua
+++ lua/smaug/config.lua
@@ -2,8 +2,8 @@ local M = {}
 
 M.theme = "github_dark_default"
 
-M.lsp_servers = { "sumneko_lua", "tsserver", "jsonls" }
+M.lsp_servers = { "sumneko_lua", "tsserver", "jsonls", "vuels" }
 
-M.treesitter =  { "lua", "javascript", "typescript" }
+M.treesitter =  { "lua", "javascript", "typescript", "vue" }
 
 return M
