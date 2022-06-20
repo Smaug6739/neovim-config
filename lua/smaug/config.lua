4d5c78d248500cebfd69750fb243b7de32794a95
Smaug6739 <veauville.raphael@gmail.com>
2022-06-20 15:09:14 +0200

    Add Go configuration

--- lua/smaug/config.lua
+++ lua/smaug/config.lua
@@ -2,8 +2,8 @@ local M = {}
 
 M.theme = "github_dark_default"
 
-M.lsp_servers = { "sumneko_lua", "tsserver", "jsonls", "vuels", "html", "css" }
+M.lsp_servers = { "sumneko_lua", "tsserver", "jsonls", "vuels", "html", "css", "gopls" }
 
-M.treesitter =  { "lua", "javascript", "typescript", "vue", "html", "css" }
+M.treesitter =  { "lua", "javascript", "typescript", "vue", "html", "css", "go" }
 
 return M
