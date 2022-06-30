097f0fdd9f217d754f72fea22b13070ac55d65cf
Smaug6739 <veauville.raphael@gmail.com>
2022-06-30 17:10:52 +0200

    Update

--- lua/smaug/general/options.lua
+++ lua/smaug/general/options.lua
@@ -27,16 +27,16 @@ vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
 vim.opt.cursorline = true                       -- highlight the current line
 vim.opt.number = true                           -- set numbered lines
 vim.opt.relativenumber = false                  -- set relative numbered lines
-vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
+vim.opt.numberwidth = 2                         -- set number column width to 2 {default 4}
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false                            -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "Hack:h12"                    -- the font used in graphical neovim applications
+vim.opt.guifont = "Hack:12"                    -- the font used in graphical neovim applications
 
 vim.opt.shortmess:append "c"
-
 --vim.cmd "set whichwrap+=<,>,[,],h,l"
 --vim.cmd [[set iskeyword+=-]]
 --vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
 
